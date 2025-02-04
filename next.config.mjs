c53ecebdbef71cacc87fe399bb5ca09de67ddcea
RizWaaN3024 <rizwaanans786@gmail.com>
2025-02-05 01:00:42 +0530

    Code fixes and tailwind css setup

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+    // Your Next.js config here
+    images: {
+        domains: ['localhost'], // Add your payload CMS domain
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3000',
+            },
+        ],
+    },
 }
 
 export default withPayload(nextConfig)
