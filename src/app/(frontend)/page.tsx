3967b7c38ae94bf10f7a47ac4236c2c9d347982d
RizWaaN3024 <rizwaanans786@gmail.com>
2025-02-07 23:23:13 +0530

    Code fixes

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -23,10 +23,7 @@ export default async function HomePage() {
 		},
 		depth: 2
 	})
-	console.log(JSON.stringify(posts, null, 4))
-
 	// const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
 	return (
 		<div className="container mx-auto px-4 py-8">
 			<h1 className="text-4xl font-bold mb-8">Blog Posts</h1>
